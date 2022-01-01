50d876f5cf78e7a066d10e49db00c919e88d47d3
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-01-01 18:42:00 +0200

    homework cycles is done, changes are made

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,37 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
         // homework 1
-           for (int i = 1; i <= 10; i++) {
+
+        int ii = 1;
+        while (ii <= 10) {
+            System.out.print(ii++);
+        }
+        System.out.println();
+
+           for (int i = 10; i >= 1; i--) {
         System.out.print(i);
            }
 
-           System.out.println();
-
-           int ii = 10;
-         while (ii >= 1) {
-            System.out.print(ii--);
-           }
-
-           System.out.println();
+         System.out.println();
 
         // homework 2
 
-            for (int weekday = 1; weekday <= 31; weekday++) {
-             if (weekday % 5 ==0) {
-           System.out.println("Сегодня пятница, " + weekday + " -е число. Необходимо подготовить отчет");
-         }
-          }
+           for (int weekday = 5; weekday <= 31; weekday++) {
+          if (weekday % 7 ==0) {
+        System.out.println("Сегодня пятница, " + weekday + " -е число. Необходимо подготовить отчет");
+        }
+           }
 
         // homework 3
 
-        int currentYear = 2021;
-        int last200Years = currentYear - 200;
-        int next100Years = currentYear + 100;
-        for (int year =  last200Years; year <= next100Years; year++) {
-            if(year % 79 ==0) {
-                System.out.println(year);
-            }
-        }
+       int currentYear = 2021;
+       int last200Years = currentYear - 200;
+       int next100Years = currentYear + 100;
+      for (int year =  last200Years; year <= next100Years; year++) {
+          if(year % 79 ==0) {
+               System.out.println(year);
+           }
+      }
     }
 }
 
