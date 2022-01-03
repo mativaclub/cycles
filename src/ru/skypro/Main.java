bd6df1a3cdddf813ac94b8aa82f9a0ffd0f301f0
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-01-03 09:20:03 +0200

    homework cycles is done, changes are done

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -7,34 +7,33 @@ public class Main {
 
         int ii = 1;
         while (ii <= 10) {
-            System.out.print(ii++);
-        }
+           System.out.print(ii++);
+       }
         System.out.println();
 
-           for (int i = 10; i >= 1; i--) {
-        System.out.print(i);
+          for (int i = 10; i >= 1; i--) {
+       System.out.print(i);
            }
 
          System.out.println();
 
         // homework 2
 
-           for (int weekday = 5; weekday <= 31; weekday++) {
-          if (weekday % 7 ==0) {
+           for (int weekday = 5; weekday <= 31; weekday+=7) {
         System.out.println("Сегодня пятница, " + weekday + " -е число. Необходимо подготовить отчет");
         }
-           }
+
 
         // homework 3
 
-       int currentYear = 2021;
-       int last200Years = currentYear - 200;
-       int next100Years = currentYear + 100;
+      int currentYear = 2021;
+           int last200Years = currentYear - 200;
+      int next100Years = currentYear + 100;
       for (int year =  last200Years; year <= next100Years; year++) {
-          if(year % 79 ==0) {
-               System.out.println(year);
-           }
-      }
+         if(year % 79 ==0) {
+              System.out.println(year);
+         }
+     }
     }
 }
 
