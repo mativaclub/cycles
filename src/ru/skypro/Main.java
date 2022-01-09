173d11127bc2ac9c30eb9505c87d383ac8a8799f
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-01-09 17:14:51 +0200

    All homeworks are done

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -26,14 +26,40 @@ public class Main {
 
         // homework 3
 
-      int currentYear = 2021;
-           int last200Years = currentYear - 200;
-      int next100Years = currentYear + 100;
-      for (int year =  last200Years; year <= next100Years; year++) {
-         if(year % 79 ==0) {
-              System.out.println(year);
-         }
+     int currentYear = 2021;
+          int last200Years = currentYear - 200;
+     int next100Years = currentYear + 100;
+     for (int year = last200Years; year <= next100Years; year++) {
+        if(year % 79 ==0) {
+             System.out.println(year);
+        }
      }
+
+// homework 4
+        for (int day = 1; day <= 30; day++) {
+            if(day % 3 ==0) {
+                System.out.println(day + " ping");
+            } else if(day % 5 ==0) {
+                System.out.println(day + " pong");
+            } else if (day % 3 ==0 && day % 5 ==0) {
+                System.out.println(day + " ping pong");
+            } else {
+                System.out.println(day);
+            }
+        }
+
+
+        // homework 5
+        int zero = 0;
+        int one = 1;
+        int nextNumber;
+        System.out.print(zero + " " + one + " ");
+        for (int fibo = 2; fibo <=9; fibo++) {
+            nextNumber = zero + one;
+            System.out.print(nextNumber + " ");
+            zero = one;
+            one = nextNumber;
+        }
     }
 }
 
